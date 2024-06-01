50648414933818d6cf37292b4eaeb9260c1324ce
Hugo <hugo22226@gmail.com>
2024-06-01 19:10:45 +0900

    chore(db): changed prisma schema to be compliant with the v0 db schema

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './modules/user/user.module';
+import { RouterModule } from '@nestjs/core';
+
+@Module({
+  imports: [
+    UserModule,
+    RouterModule.register([
+        {
+          path: 'users',
+          module: UserModule,
+        },
+      ]),
+  ],
+})
+
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { UserModule } from './modules/user/user.module';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UserModule);
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
   await app.listen(3000);
 }
 bootstrap();
