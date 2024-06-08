7763ac4ed30e538e0a051f64a26f946a269854b5
Hugo <hugo22226@gmail.com>
2024-06-08 19:59:33 +0900

    chore: Added test structure to project

---
+++ test/unit/modules/user/user.controller.spec.ts
@@ -0,0 +1,52 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from '../../../../src/modules/user/user.controller';
+import { UserService } from '../../../../src/modules/user/user.service';
+import { UserRepository } from '../../../../src/modules/user/user.repository';
+import { PrismaService } from '../../../../src/prisma/prisma.service';
+import { UserMapper } from '../../../../src/modules/user/dtos/user.mapper';
+import { PasswordEncryption } from '../../../../src/common/passwordEncryption';
+import {
+  defaultCreateUserDto,
+  defaultCreateUserResponseDto,
+} from './user.utils';
+
+describe('UserController', () => {
+  let userController: UserController;
+  let userService: UserService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        UserService,
+        UserRepository,
+        PrismaService,
+        UserMapper,
+        PasswordEncryption,
+      ],
+    }).compile();
+
+    userController = app.get<UserController>(UserController);
+    userService = app.get<UserService>(UserService);
+  });
+
+  describe('createUser function', () => {
+    it('should return a correct responseDto', async () => {
+      // Mock call to Dto validator
+      const validatorSpy = jest.spyOn(userService, 'IsUserCreateDtoValid');
+
+      // Mock call to DB
+      const mainSpy = jest
+        .spyOn(userService, 'createUser')
+        .mockResolvedValue(defaultCreateUserResponseDto);
+      userService;
+
+      const response = await userController.createUser(defaultCreateUserDto);
+
+      expect(validatorSpy).toHaveBeenCalledTimes(1);
+      expect(mainSpy).toHaveBeenCalledTimes(1);
+      expect(mainSpy).toHaveBeenCalledWith(defaultCreateUserDto);
+      expect(response).toBe(defaultCreateUserResponseDto);
+    });
+  });
+});
