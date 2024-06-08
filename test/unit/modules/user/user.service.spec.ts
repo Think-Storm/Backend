f82047207cce9a5addd560aff441d6f2ee158549
Hugo <hugo22226@gmail.com>
2024-06-08 19:59:33 +0900

    chore: Added test structure to project

---
+++ test/unit/modules/user/user.service.spec.ts
@@ -0,0 +1,108 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from '../../../../src/modules/user/user.service';
+import { defaultCreateUserDto, defaultUser } from './user.utils';
+import { UserRepository } from '../../../../src/modules/user/user.repository';
+import { PasswordEncryption } from '../../../../src/common/passwordEncryption';
+import { UserMapper } from '../../../../src/modules/user/dtos/user.mapper';
+import { UserController } from '../../../../src/modules/user/user.controller';
+import { PrismaService } from '../../../../src/prisma/prisma.service';
+import { BadRequestException } from '@nestjs/common';
+import { defaultSaltAndPassword } from '../../common/passwordEncryption.utils';
+import { CreateUserResponseDto } from '../../../../src/modules/user/dtos/createUserResponse.dto';
+
+describe('UserService', () => {
+  let userService: UserService;
+  let userRepository: UserRepository;
+  let passwordEncryption: PasswordEncryption;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        UserService,
+        UserRepository,
+        PasswordEncryption,
+        UserMapper,
+        PrismaService,
+      ],
+    }).compile();
+
+    userService = module.get<UserService>(UserService);
+    userRepository = module.get<UserRepository>(UserRepository);
+    passwordEncryption = module.get<PasswordEncryption>(PasswordEncryption);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('IsUserCreateDtoValid function', () => {
+    it('should throw an exception if user already exists with email', async () => {
+      // Mock call to DB to return a User
+      const spy = jest
+        .spyOn(userRepository, 'getUserByEmail')
+        .mockResolvedValue(defaultUser);
+
+      expect(
+        userService.IsUserCreateDtoValid(defaultCreateUserDto),
+      ).rejects.toThrow(BadRequestException);
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not throw an exception if no user exists with email', async () => {
+      // Mock call to DB not to return a User
+      const spy = jest
+        .spyOn(userRepository, 'getUserByEmail')
+        .mockResolvedValue(null);
+
+      expect(() =>
+        userService.IsUserCreateDtoValid(defaultCreateUserDto),
+      ).not.toThrow();
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('createUser function', () => {
+    it('should create user and map the result into CreateUserResponseDto', async () => {
+      // Mock call to the password and salt creation
+      const passwordSpy = jest
+        .spyOn(passwordEncryption, 'createSaltAndHashedPassword')
+        .mockResolvedValue(defaultSaltAndPassword);
+
+      // Mock call to DB
+      const dbSpy = jest
+        .spyOn(userRepository, 'createUser')
+        .mockResolvedValue(defaultUser);
+
+      const expectedResponseDto: CreateUserResponseDto = {
+        id: defaultUser.id,
+        email: defaultUser.email,
+        username: defaultUser.username,
+        fullName: defaultUser.fullName,
+        birthdate: defaultUser.birthdate,
+        avatar: defaultUser.avatar,
+        bio: defaultUser.bio,
+        createdAt: defaultUser.createdAt,
+        lastUpdatedAt: defaultUser.lastUpdatedAt,
+      };
+
+      const createUserResponseDto =
+        await userService.createUser(defaultCreateUserDto);
+
+      // Checking the mapper
+      expect(createUserResponseDto).toEqual(expectedResponseDto);
+      expect(createUserResponseDto).not.toHaveProperty('password');
+      expect(createUserResponseDto).not.toHaveProperty('passwordSalt');
+
+      expect(passwordSpy).toHaveBeenCalledTimes(1);
+
+      expect(dbSpy).toHaveBeenCalledTimes(1);
+      expect(dbSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          password: defaultSaltAndPassword.hashedPassword,
+        }),
+        defaultSaltAndPassword.passwordSalt,
+      );
+    });
+  });
+});
---
+++ test/unit/modules/user/user.utils.ts
@@ -0,0 +1,38 @@
+import { User } from '@prisma/client';
+import { CreateUserDto } from '../../../../src/modules/user/dtos/createUser.dto';
+import { CreateUserResponseDto } from '../../../../src/modules/user/dtos/createUserResponse.dto';
+
+export const defaultCreateUserDto: CreateUserDto = {
+  username: 'username',
+  fullName: 'Full Name',
+  email: 'email@email.com',
+  password: 'hashedPassword',
+  birthdate: new Date('2000-01-01'),
+  bio: 'bio',
+};
+
+export const defaultCreateUserResponseDto: CreateUserResponseDto = {
+  id: 0,
+  username: 'username',
+  fullName: 'Full Name',
+  email: 'email@email.com',
+  birthdate: new Date('2000-01-01'),
+  bio: 'bio',
+  avatar: 'avatar',
+  createdAt: new Date('2000-01-01'),
+  lastUpdatedAt: new Date('2000-01-01'),
+};
+
+export const defaultUser: User = {
+  id: 1,
+  email: 'email@email.com',
+  username: 'username',
+  password: 'hashedPassword',
+  passwordSalt: 'passwordsalt',
+  fullName: 'Full Name',
+  birthdate: new Date('2000-01-01'),
+  avatar: 'avatar',
+  bio: 'bio',
+  createdAt: new Date('2000-01-01'),
+  lastUpdatedAt: new Date('2000-01-01'),
+};
