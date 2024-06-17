8a643fa6930a7624db7a892ace8bfaa678588e01
kimdahee0815 <billionairedahee@gmail.com>
2024-06-17 12:39:43 +0900

    feat: userController getUser function

---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,12 @@
+export class User {
+  id: number;
+  email: string;
+  username: string;
+  password: string;
+  fullName: string;
+  birthdate: Date;
+  avatar?: string;
+  bio?: string;
+  //foundedProjects
+  //projects
+}
