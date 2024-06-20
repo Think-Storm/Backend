893e16d052a8523b9babb0ef9e71b9f6f633ad24
kimdahee0815 <billionairedahee@gmail.com>
2024-06-20 10:23:14 +0900

    fix: error class fix for parameter in constructor

--- src/common/appError.ts
+++ src/common/appError.ts
@@ -1,12 +1,12 @@
 export class AppError extends Error {
+  status: string;
+  isOperational: boolean;
   constructor(
     public message: string,
     private statusCode: number,
-    private status: string,
-    private isOperational: boolean,
   ) {
     super(message);
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.status = `${this.statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
