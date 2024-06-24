08dc1e5e394fe79b9bc6a4ec4a604b18fd168dec
kimdahee0815 <billionairedahee@gmail.com>
2024-06-24 11:50:03 +0900

    test: unit test for repository getUser

--- test/unit/modules/user/user.repository.spec.ts
+++ test/unit/modules/user/user.repository.spec.ts
@@ -3,6 +3,8 @@ import { PrismaService } from '../../../../src/prisma/prisma.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { defaultCreateUserDto } from './user.utils';
 import { defaultPasswordSalt } from '../../common/passwordEncryption.utils';
+import { ServiceException } from 'src/common/exception-filter/serviceException';
+import { errorMessages } from 'src/common/enums/errorMessages';
 
 describe('UserRepository', () => {
   let prismaService: PrismaService;
@@ -77,6 +79,45 @@ describe('UserRepository', () => {
       const user = await userRepository.getUserByEmail(fakeEmail);
 
       expect(user).toBeNull();
+    });
+  });
+
+  describe('getUser function', () => {
+    it('should get a searched user in DB', async () => {
+      // create a user
+      await userRepository.createUser(
+        defaultCreateUserDto,
+        defaultPasswordSalt,
+      );
+
+      const user = await userRepository.getUser(1);
+
+      expect(user).toHaveProperty('id');
+      expect(user.createdAt).toBeDefined();
+      expect(user.lastUpdatedAt).toBeDefined();
+      expect(user.username).toBe(defaultCreateUserDto.username);
+      expect(user.fullName).toBe(defaultCreateUserDto.fullName);
+      expect(user.email).toBe(defaultCreateUserDto.email);
+      expect(user.password).toBe(defaultCreateUserDto.password);
+      expect(user.birthdate.toDateString()).toBe(
+        defaultCreateUserDto.birthdate.toDateString(),
+      );
+      expect(user.bio).toBe(defaultCreateUserDto.bio);
+      expect(user.passwordSalt).toBe(defaultPasswordSalt);
+    });
+
+    it('should fail if userId is String type', async () => {
+      // create a user
+      await userRepository.createUser(
+        defaultCreateUserDto,
+        defaultPasswordSalt,
+      );
+      try {
+        await userRepository.getUser(Number('abc'));
+      } catch (e) {
+        expect(e).toBeInstanceOf(ServiceException);
+        expect(e.message).toBe(errorMessages.VALIDATION_ERROR);
+      }
     });
   });
 });
