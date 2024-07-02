5caa3fd12bb0cc87ca36e61a64f7963659de1346
kimdahee0815 <billionairedahee@gmail.com>
2024-07-02 13:08:41 +0900

    chore: createUserResponseDto name changed to userResponseDto

--- src/modules/user/dtos/user.mapper.ts
+++ src/modules/user/dtos/user.mapper.ts
@@ -1,15 +1,15 @@
 import { User } from '@prisma/client';
-import { CreateUserResponseDto } from './createUserResponse.dto';
+import { UserResponseDto } from './userResponse.dto';
 import { instanceToPlain, plainToInstance } from 'class-transformer';
 
 export class UserMapper {
   /**
-   * Maps a User entity to a CreateUserResponseDto
+   * Maps a User entity to a UserResponseDto
    * @param user - The User entity to be mapped
-   * @returns A CreateUserResponseDto with the mapped data
+   * @returns A UserResponseDto with the mapped data
    */
-  userToCreateUserResponseDTO(user: User): CreateUserResponseDto {
-    return plainToInstance(CreateUserResponseDto, instanceToPlain(user), {
+  userToUserResponseDTO(user: User): UserResponseDto {
+    return plainToInstance(UserResponseDto, instanceToPlain(user), {
       excludeExtraneousValues: true,
     });
   }
