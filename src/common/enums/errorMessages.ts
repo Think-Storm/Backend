47ab5bbfe6919b8d7c1000b4bf3827b8fd81b6cd
kimdahee0815 <billionairedahee@gmail.com>
2024-07-02 13:17:32 +0900

    feat: create global error filter for error handling

--- src/common/enums/errorMessages.ts
+++ src/common/enums/errorMessages.ts
@@ -1,5 +1,7 @@
 export const errorMessages = {
   USER_WITH_EMAIL_ALREADY_EXISTS: 'This email is already used by another user.',
   ERROR_CREATING_USER_IN_DB: 'An error occured while inserting User in DB.',
-  PROJECT_NOT_FOUND: (id: string) => `The project with id ${id} was not found.`,
+  SERVER_ERROR: 'An error occured in server.',
+  VALIDATION_ERROR: 'Validation is failed.',
+  ENTITY_NOT_FOUND: (id: string) => `Entity with id ${id} was not found.`,
 };
