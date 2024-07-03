b1074eea7c2706fec9ba047fd9a4dfe67d83764c
kimdahee0815 <billionairedahee@gmail.com>
2024-07-03 13:21:32 +0900

    refactor: authService login checkUserAndPassword method

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -18,6 +18,21 @@ export class AuthService {
     private userMapper: UserMapper,
   ) {}
 
+  checkUserAndPassword = async (email, password) => {
+    const user = await this.userRepository.getUserByEmail(email);
+
+    if (
+      !user ||
+      !(await this.passwordEncryption.correctPassword(password, user.password))
+    ) {
+      throw ServiceException.AuthException(
+        errorMessages.INCORRECT_EMAIL_OR_PASSWORD,
+      );
+    }
+
+    return user;
+  };
+
   signToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRES_IN,
@@ -30,20 +45,11 @@ export class AuthService {
       throw new BadRequestException(errorMessages.BAD_REQUEST_LOGIN_ERROR);
     }
 
-    // 2) Check if user exists && password is correct (validate User)
-    const user = await this.userRepository.getUserByEmail(loginUserDto.email);
-
-    if (
-      !user ||
-      !(await this.passwordEncryption.correctPassword(
-        loginUserDto.password,
-        user.password,
-      ))
-    ) {
-      throw ServiceException.AuthException(
-        errorMessages.INCORRECT_EMAIL_OR_PASSWORD,
-      );
-    }
+    // 2) Check if user exists && password is correct
+    const user = await this.checkUserAndPassword(
+      loginUserDto.email,
+      loginUserDto.password,
+    );
 
     // 3) If everything is okay, send jwt token via cookie
     this.signToken(user.id);
