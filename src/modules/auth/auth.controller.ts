cd97f6bac97202e48be1d75c4d4f668fc7f61a63
kimdahee0815 <billionairedahee@gmail.com>
2024-07-03 16:22:08 +0900

    feat: userController login jwt token to cookie and header bearer

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,38 @@
 import { AuthService } from './auth.service';
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { loginUserDto } from './dtos/loginUser.dto';
-import { UserResponseDto } from '../user/dtos/userResponse.dto';
+// import { UserResponseDto } from '../user/dtos/userResponse.dto';
+import { Response } from 'express';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
-  async login(@Body() body: loginUserDto): Promise<UserResponseDto> {
-    return this.authService.login(body);
+  async login(@Body() body: loginUserDto, @Res() res: Response): Promise<any> {
+    const loginSuccessDto = await this.authService.login(body);
+    res.setHeader('Authorization', 'Bearer ' + loginSuccessDto.token);
+
+    const cookieOptions = {
+      httpOnly: true,
+      secure: false,
+      expires: new Date(
+        Date.now() +
+          Number(process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 1000,
+      ),
+    };
+    if (process.env.NODE_ENV === 'production') {
+      cookieOptions.secure = true;
+    }
+
+    res.cookie('jwt', loginSuccessDto.token, cookieOptions);
+
+    console.log(res.cookie);
+    console.log(loginSuccessDto);
+
+    return res.send({
+      message: 'success',
+      data: loginSuccessDto,
+    });
   }
 }
