52d94088c21c63db2d4866eaa28d66eac84645a9
kimdahee0815 <billionairedahee@gmail.com>
2024-07-03 16:24:34 +0900

    fix: authRepository change

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -4,6 +4,4 @@ import { PrismaService } from '../../prisma/prisma.service';
 @Injectable()
 export class AuthRepository {
   constructor(private readonly prisma: PrismaService) {}
-
-  async login() {}
 }
