160f2b2c7b6e257e63c4a61ab66826c35f63c50b
kimdahee0815 <billionairedahee@gmail.com>
2024-07-03 11:10:50 +0900

    feat: authController login function

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,13 @@
 import { AuthService } from './auth.service';
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { loginUserDto } from './dtos/loginUser.dto';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/login')
+  async login(@Body() body: loginUserDto) {
+    return this.authService.login(body);
+  }
 }
