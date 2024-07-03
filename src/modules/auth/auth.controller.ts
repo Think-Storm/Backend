ecfe252aa5a2707775caf28661ec305239f5206c
kimdahee0815 <billionairedahee@gmail.com>
2024-07-03 11:32:08 +0900

    feat: authController login function

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { AuthService } from './auth.service';
 import { Controller, Post, Body } from '@nestjs/common';
 import { loginUserDto } from './dtos/loginUser.dto';
+import { UserResponseDto } from '../user/dtos/userResponse.dto';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
-  async login(@Body() body: loginUserDto) {
+  async login(@Body() body: loginUserDto): Promise<UserResponseDto> {
     return this.authService.login(body);
   }
 }
