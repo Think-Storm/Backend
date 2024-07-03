a67965995a27f63b3bbe973860001e87906206b8
kimdahee0815 <billionairedahee@gmail.com>
2024-07-03 14:37:26 +0900

    feat: auth module created

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { PasswordEncryption } from '../../common/passwordEncryption';
+import { UserMapper } from '../user/dtos/user.mapper';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { AuthRepository } from './auth.repository';
+
+/**
+ * Module for user-related components and services
+ */
+@Module({
+  imports: [],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    AuthRepository,
+    PrismaService,
+    PasswordEncryption,
+    UserMapper,
+  ],
+})
+export class UserModule {}
