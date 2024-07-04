03af1507e4b2e39c82086ee73a886d0bd15b5f46
kimdahee0815 <billionairedahee@gmail.com>
2024-07-04 13:55:35 +0900

    feat: authService login with cookie

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -75,6 +75,8 @@ export class AuthService {
       req.headers.authorization.startsWith('Bearer')
     ) {
       token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.jwt) {
+      token = req.cookies.jwt;
     }
 
     if (!token) {
