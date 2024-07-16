4d7e2669fcc6597c7b3c633db7cfda21c3d943ea
kimdahee0815 <billionairedahee@gmail.com>
2024-07-16 11:56:58 +0900

    fix: remove mockAuthGuard.util.ts

--- test/unit/modules/auth/guard/mockAuthGuard.util.ts
+++
@@ -1,9 +0,0 @@
-import { ExecutionContext } from '@nestjs/common';
-
-export const mockAuthGuard = {
-  canActivate: (context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
-    request['user'] = { id: 1 };
-    return true;
-  },
-};
