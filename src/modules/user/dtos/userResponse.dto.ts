bbe88e5f5ac6b32202e79ab07a995940ec86cad1
kimdahee0815 <billionairedahee@gmail.com>
2024-07-17 12:05:00 +0900

    refactor: passwordChangedAt field exclude

--- src/modules/user/dtos/userResponse.dto.ts
+++ src/modules/user/dtos/userResponse.dto.ts
@@ -1,4 +1,4 @@
-import { Expose } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 
 export class UserResponseDto {
   @Expose()
@@ -9,6 +9,9 @@ export class UserResponseDto {
 
   @Expose()
   username: string;
+
+  @Exclude()
+  password: string;
 
   @Expose()
   fullName?: string;
