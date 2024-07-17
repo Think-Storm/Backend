4204201466329a0fdbd71cf40259fcd709cf0877
kimdahee0815 <billionairedahee@gmail.com>
2024-07-17 12:03:26 +0900

    feat: add authorization login API

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import { AuthService } from './auth.service';
+import {
+  Controller,
+  Post,
+  Res,
+  UseGuards,
+  Req,
+  HttpCode,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { LocalAuthGuard } from './local/local.guard';
+import RequestWithUser from './local/requestWithUser.interface';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(200)
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async login(@Req() req: RequestWithUser, @Res() res: Response): Promise<any> {
+    const user = req.user;
+    const loggedInUser = this.authService.authentication(user, res);
+
+    return res.send({
+      message: 'success',
+      data: loggedInUser,
+    });
+  }
+}
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { PasswordEncryption } from '../../common/passwordEncryption';
+import { UserMapper } from '../user/dtos/user.mapper';
+import { AuthRepository } from './auth.repository';
+import { UserRepository } from './../user/user.repository';
+import { errorMessages } from '../../../src/common/enums/errorMessages';
+import { ServiceException } from './../../common/exception-filter/serviceException';
+import { JwtService } from '@nestjs/jwt';
+import { UserResponseDto } from '../user/dtos/userResponse.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private authRepository: AuthRepository,
+    private userRepository: UserRepository,
+    private passwordEncryption: PasswordEncryption,
+    private userMapper: UserMapper,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  checkUserAndPassword = async (email: string, password: string) => {
+    const user = await this.userRepository.getUserByEmail(email);
+
+    if (
+      !user ||
+      !(await this.passwordEncryption.correctPassword(password, user.password))
+    ) {
+      throw ServiceException.AuthException(
+        errorMessages.INCORRECT_EMAIL_OR_PASSWORD,
+      );
+    }
+
+    return user;
+  };
+
+  signToken = (id: number) => {
+    return this.jwtService.sign({ id });
+  };
+
+  verifyToken = async (token: string, secret: string) => {
+    return await this.jwtService.verify(token, { secret });
+  };
+
+  getToken(user: UserResponseDto): string {
+    const token = this.signToken(user.id);
+
+    return token;
+  }
+
+  authentication(user: UserResponseDto, @Res() res: Response): UserResponseDto {
+    const token = this.getToken(user);
+    res.setHeader('Authorization', 'Bearer ' + token);
+
+    const cookieOptions = {
+      httpOnly: true,
+      secure: false,
+      expires: new Date(
+        Date.now() +
+          Number(process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 1000,
+      ),
+    };
+    if (process.env.NODE_ENV === 'production') {
+      cookieOptions.secure = true;
+    }
+
+    res.cookie('jwt', token, cookieOptions);
+
+    return user;
+  }
+}
