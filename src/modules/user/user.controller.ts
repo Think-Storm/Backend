1a7b8e0ab9247de98b75a6e63e30db7cccca27d5
kimdahee0815 <billionairedahee@gmail.com>
2024-07-17 12:05:00 +0900

    refactor: passwordChangedAt field exclude

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,28 @@
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/createUser.dto';
-import { Controller, Get, Post, Body, Param, HttpCode } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  HttpCode,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  //UseGuards,
+} from '@nestjs/common';
 import { UserResponseDto } from './dtos/userResponse.dto';
+import { AuthService } from '../auth/auth.service';
+//import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
 
 @Controller()
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @HttpCode(201)
   @Post()
   async createUser(@Body() body: CreateUserDto) {
@@ -14,6 +30,8 @@ export class UserController {
     return this.userService.createUser(body);
   }
 
+  //@UseGuards(JwtAuthGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async getUserById(@Param('id') userId: number): Promise<UserResponseDto> {
     return this.userService.getUserById(userId);
