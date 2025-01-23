aa009d9858cc5f352472d601001a9aafccac0bf0
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-01-24 05:13:20 +0900

    fix: fix module source

--- src/modules/common-data/common-data.module.ts
+++ src/modules/common-data/common-data.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { CommonDataService } from './common-data.service';
 import { CommonDataController } from './common-data.controller';
 import { CommonDataRepository } from './common-data.repository';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { CommonDataMapper } from './dto/common-data.mapper';
 
 @Module({
