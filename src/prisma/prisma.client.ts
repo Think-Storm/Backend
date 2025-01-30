fc453e3999c9a0db9d283ca2a089c75b5760a19f
kimdahee0815 <billionairedahee@gmail.com>
2025-01-30 11:42:02 +0900

    feat: omit for password, passwordChangedAt, passwordSalt field globally

--- src/prisma/prisma.client.ts
+++ src/prisma/prisma.client.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  return new PrismaClient({
+    omit: {
+      user: {
+        password: true,
+        passwordChangedAt: true,
+        passwordSalt: true,
+      },
+    },
+  });
 };
 
 declare const globalThis: {
