081026a7982b101337550bc6c1b360d7e778b042
HARIAN Elyoth <harian.elyoth@outlook.com>
2025-01-31 13:40:54 +0100

    feat: add e2e test for update project api

--- test/e2e/project.e2e-spec.ts
+++ test/e2e/project.e2e-spec.ts
@@ -109,4 +109,142 @@ describe('/projects', () => {
       return request(app.getHttpServer()).get(`/${invalidId}`).expect(400);
     });
   });
+
+  describe('/:id PUT (Update Project)', () => {
+    it('should return a 204 if everything is fine', async () => {
+      // First create a user
+      await request(app.getHttpServer())
+        .post('/register')
+        .send(defaultCreateUserDto)
+        .expect(201);
+
+      // Then create a project with that user as founder
+      const createdProject = await createProjectInDB(
+        prismaService,
+        defaultCreateProjectDto,
+      );
+
+      const loginResponse = await request(app.getHttpServer())
+        .post('/login')
+        .send({
+          email: defaultCreateUserDto.email,
+          password: defaultCreateUserDto.password,
+        })
+        .expect(200);
+
+      const validUpdateRequest = { ...defaultUpdateProjectDto };
+      validUpdateRequest.founderId = createdProject.founderId;
+
+      const token = loginResponse.headers.authorization;
+
+      await request(app.getHttpServer())
+        .put(`/${createdProject.id}`)
+        .set('Authorization', token)
+        .send(validUpdateRequest)
+        .expect(204);
+
+      const updatedProject = await prismaService.project.findUnique({
+        where: { id: createdProject.id },
+      });
+
+      const updatedDomainLabels = await prismaService.projectDomainLabel
+        .findMany({
+          where: { projectId: createdProject.id },
+        })
+        .then((labels) => labels.map((label) => label.labelName).sort());
+
+      const updatedTechnicalLabels = await prismaService.projectTechnicalLabel
+        .findMany({
+          where: { projectId: createdProject.id },
+        })
+        .then((labels) => labels.map((label) => label.labelName).sort());
+
+      expect(updatedProject).not.toBeNull();
+      expect(updatedProject.title).toBe(validUpdateRequest.title);
+      expect(updatedProject.description).toBe(validUpdateRequest.description);
+      expect(updatedProject.languageCode).toBe(validUpdateRequest.languageCode);
+      expect(updatedProject.founderId).toBe(validUpdateRequest.founderId);
+      expect(updatedProject.status).toBe(validUpdateRequest.status);
+      expect(updatedProject.milestone).toStrictEqual(
+        validUpdateRequest.milestone,
+      );
+      expect(updatedProject.goal).toBe(validUpdateRequest.goal);
+      expect(updatedDomainLabels).toStrictEqual(
+        validUpdateRequest.domainLabels.sort(),
+      );
+      expect(updatedTechnicalLabels).toStrictEqual(
+        validUpdateRequest.technicalLabels.sort(),
+      );
+    });
+
+    it('should return a 404 if project does not exist', async () => {
+      await request(app.getHttpServer())
+        .post('/register')
+        .send(defaultCreateUserDto)
+        .expect(201);
+
+      await createProjectInDB(prismaService, defaultCreateProjectDto);
+
+      const loginResponse = await request(app.getHttpServer())
+        .post('/login')
+        .send({
+          email: defaultCreateUserDto.email,
+          password: defaultCreateUserDto.password,
+        })
+        .expect(200);
+
+      const token = loginResponse.headers.authorization;
+      const fakeId = 999;
+      const nonExistentProjectUpdateRequest = { ...defaultUpdateProjectDto };
+      nonExistentProjectUpdateRequest.id = fakeId;
+
+      return request(app.getHttpServer())
+        .put(`/${fakeId}`)
+        .send(nonExistentProjectUpdateRequest)
+        .set('Authorization', token)
+        .expect(404);
+    });
+
+    it('should return a 403 if user is not the owner of the project', async () => {
+      // Create original project with its owner
+      await request(app.getHttpServer())
+        .post('/register')
+        .send(defaultCreateUserDto)
+        .expect(201);
+
+      const originalProject = await createProjectInDB(
+        prismaService,
+        defaultCreateProjectDto,
+      );
+
+      // Create another user who will try to update the project
+      const newUserDto = defaultCreateUserDto;
+      newUserDto.email = 'newUser@email.com';
+      newUserDto.password = 'newUserPassword';
+      await request(app.getHttpServer())
+        .post('/register')
+        .send(newUserDto)
+        .expect(201);
+
+      // Login with the unauthorized user
+      const loginResponse = await request(app.getHttpServer())
+        .post('/login')
+        .send({
+          email: newUserDto.email,
+          password: 'newUserPassword',
+        })
+        .expect(200);
+
+      // Get the token from response headers
+      const token = loginResponse.headers.authorization;
+
+      const unauthorizedUpdateRequest = { ...defaultUpdateProjectDto };
+
+      return request(app.getHttpServer())
+        .put(`/${originalProject.id}`)
+        .set('Authorization', token)
+        .send(unauthorizedUpdateRequest)
+        .expect(403);
+    });
+  });
 });
