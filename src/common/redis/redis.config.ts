32e3fac3b7b3dcd170bfaab5df0bf4533a479b8c
kimdahee0815 <billionairedahee@gmail.com>
2025-02-20 10:20:06 +0900

    fix: redis caching ttl type change to number and add redis password

--- src/common/redis/redis.config.ts
+++ src/common/redis/redis.config.ts
@@ -5,7 +5,8 @@ export function throttlerConfig(configService: ConfigService) {
     host: configService.get<string>('REDIS_HOST'), // Use ConfigService to get environment variables
     port: configService.get<number>('REDIS_PORT'),
     db: configService.get<number>('REDIS_THROTTLER_DB'),
-    //password: configService.get<string>('REDIS_PASSWORD'),
+    username: configService.get<string>('REDIS_USER'),
+    password: configService.get<string>('REDIS_PASSWORD'),
   };
 }
 
@@ -13,7 +14,8 @@ export function cachingConfig(configService: ConfigService) {
   return {
     host: configService.get<string>('REDIS_HOST'), // Use ConfigService to get environment variables
     port: configService.get<number>('REDIS_PORT'),
-    //password: configService.get<string>('REDIS_PASSWORD'),
+    username: configService.get<string>('REDIS_USER'),
+    password: configService.get<string>('REDIS_PASSWORD'),
     db: configService.get<number>('REDIS_CACHING_DB'),
     ttl: configService.get<number>('REDIS_CACHING_TTL'),
     retryStrategy: (times) => Math.min(times * 50, 2000),
