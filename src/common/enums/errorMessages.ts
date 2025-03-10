26839bd589063e33275d5c2f59fad93c249f37a7
kimdahee0815 <billionairedahee@gmail.com>
2025-03-10 23:38:59 +0900

    feat: add delete project api

--- src/common/enums/errorMessages.ts
+++ src/common/enums/errorMessages.ts
@@ -6,6 +6,7 @@ export const errorMessages = {
   ERROR_SEARCHING_PROJECTS:
     'An error occurred while searching projects for your queries.',
   ERROR_UPDATING_PROJECTS: 'An error occurred while updating project.',
+  ERROR_DELETING_PROJECTS: 'An error occurred while deleting project.',
   ERROR_FINDING_PROJECTS: 'An error occurred while finding projects.',
   ERROR_GETTING_USER_BY_EMAIL: 'An error occurred while getting user by email.',
   ERROR_GETTING_USER_BY_ID: 'An error occurred while getting user by id.',
