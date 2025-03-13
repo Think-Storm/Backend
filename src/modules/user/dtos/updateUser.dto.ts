03d124c2d084be4cf853ab1caa28b812529b24fe
kimdahee0815 <billionairedahee@gmail.com>
2025-03-13 16:53:01 +0900

    feat: add update user api

---
+++ src/modules/user/dtos/updateUser.dto.ts
@@ -0,0 +1,51 @@
+import { Transform } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsEmail,
+  IsOptional,
+  IsNotEmpty,
+  IsDate,
+  IsNumber,
+} from 'class-validator';
+
+export class UpdateUserDto {
+  @ApiProperty({ description: 'User ID', required: true })
+  @IsNumber()
+  @IsNotEmpty()
+  id: number;
+
+  @ApiProperty({ description: 'New username of the user', required: true })
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @ApiProperty({ description: 'New email address of the user', required: true })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    description: 'New password for the user account',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  password: string;
+
+  @ApiProperty({ description: 'New full name of the user', required: true })
+  @IsString()
+  @IsNotEmpty()
+  fullName: string;
+
+  @ApiProperty({
+    description: 'New birthdate of the user',
+    required: true,
+    type: String,
+    format: 'date-time',
+  })
+  @IsDate()
+  @Transform(({ value }) => new Date(value))
+  @IsNotEmpty()
+  birthdate: Date;
+}
