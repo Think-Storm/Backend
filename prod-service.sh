bfefd1560f034b41eaf702ff8391841b129c6e6d
Elyoth HARIAN <harian.elyoth@outlook.com>
2025-03-17 17:06:54 +0100

    ci: remove the build is the image is already here and add a clean to reset the image

--- prod-service.sh
+++ prod-service.sh
@@ -7,6 +7,7 @@ show_help() {
     echo "  start    Start the production environment containers"
     echo "  stop     Stop and remove the production environment containers"
     echo "  help     Display this help message"
+    echo "  clean    Remove the Docker image"
 }
 
 # Check if an argument is provided
@@ -18,17 +19,36 @@ fi
 # Get the command argument
 command=$1
 
+check_image_exists() {
+  if [[ "$(docker images -q thinkstorm-backend:latest 2> /dev/null)" == "" ]]; then
+    echo "Building the image..."
+    docker compose -f docker/docker-compose.app.yml build app
+  else
+    echo "Image already exists. Skipping build."
+  fi
+}
+
 case $command in
     "start")
         # Launch docker-compose with both base and production configurations
-        docker compose -f docker/docker-compose.base.yml -f docker/docker-compose.prod.yml up -d
+        check_image_exists
+        docker compose -f docker/docker-compose.db.yml -f docker/docker-compose.prod.yml up -d
         ;;
     "stop")
         # Stop and remove containers
-        docker compose -f docker/docker-compose.base.yml -f docker/docker-compose.prod.yml down
+        docker compose -f docker/docker-compose.db.yml -f docker/docker-compose.prod.yml down
         ;;
     "help")
         show_help
+        ;;
+    "clean")
+        # Remove the Docker image
+        if [[ "$(docker images -q thinkstorm-backend:latest 2> /dev/null)" != "" ]]; then
+            docker rmi thinkstorm-backend:latest
+            echo "Image removed successfully."
+        else
+            echo "Image does not exist. Nothing to remove."
+        fi
         ;;
     *)
         echo "Invalid command. Use 'start', 'stop', or 'help'"
