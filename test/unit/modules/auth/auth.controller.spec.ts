406397509d7e96ec89bd5eb814b7ad3980f754a2
kimdahee0815 <billionairedahee@gmail.com>
2025-03-28 20:19:13 +0900

    feat: add logout unit test

--- test/unit/modules/auth/auth.controller.spec.ts
+++ test/unit/modules/auth/auth.controller.spec.ts
@@ -3,9 +3,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const httpMocks = require('node-mocks-http');
 import { JwtModule } from '@nestjs/jwt';
-import { PasswordEncryption } from '../../../../src/common/encryption/passwordEncryption';
 import {
   defaultCreateUserDto,
+  defaultUser,
   defaultUserResponseDto,
 } from '../../../utils/user.utils';
 import { AuthController } from '../../../../src/modules/auth/auth.controller';
@@ -19,35 +19,18 @@ import { PassportModule } from '@nestjs/passport';
 import { UserService } from '../../../../src/modules/user/user.service';
 import { UserRepository } from '../../../../src/modules/user/user.repository';
 import { UserMapper } from '../../../../src/modules/user/dtos/user.mapper';
-import { JwtAuthGuard } from '../../../../src/modules/auth/jwt/jwt.guard';
 import { JwtStrategy } from '../../../../src/modules/auth/jwt/jwt.strategy';
 import { errorMessages } from '../../../../src/common/enums/errorMessages';
 import { ServiceException } from '../../../../src/common/exception-filter/serviceException';
+import { PasswordEncryption } from '../../../../src/common/encryption/passwordEncryption';
 
 describe('AuthController', () => {
   let authController: AuthController;
   let authService: AuthService;
-  let userRepository: UserRepository;
-
-  const mockUserRepository = {
-    getUserById: jest.fn().mockResolvedValue(defaultUserResponseDto),
-    getUserByEmail: jest.fn().mockResolvedValue(defaultUserResponseDto),
-    createUser: jest.fn().mockResolvedValue(defaultUserResponseDto),
-  };
-
-  const mockJwtStrategy = {
-    validate: jest.fn().mockResolvedValue(defaultUserResponseDto),
-    checkUserExistsInDB: jest.fn().mockResolvedValue(defaultUserResponseDto),
-    checkTokenExists: jest.fn().mockReturnValue('valid-token'),
-    verifyAndDecodeToken: jest.fn().mockResolvedValue({
-      id: defaultUserResponseDto.id,
-      iat: Date.now() / 1000,
-    }),
-    checkUserPasswordChanged: jest.fn().mockResolvedValue(undefined),
-  };
+  let jwtStrategy: JwtStrategy;
 
   beforeAll(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       imports: [
         PrismaModule.forTest(prisma),
         ConfigModule.forRoot({ isGlobal: true }),
@@ -65,29 +48,21 @@ describe('AuthController', () => {
       controllers: [AuthController],
       providers: [
         UserService,
-        {
-          provide: UserRepository,
-          useValue: mockUserRepository,
-        },
-        UserMapper,
+        UserRepository,
         PasswordEncryption,
+        UserMapper,
         AuthService,
         ConfigService,
-        {
-          provide: JwtStrategy,
-          useValue: mockJwtStrategy,
-        },
+        JwtStrategy,
       ],
     })
       .overrideGuard(LocalAuthGuard)
       .useValue({ canActivate: jest.fn().mockReturnValue(true) })
-      .overrideGuard(JwtAuthGuard)
-      .useValue({ canActivate: jest.fn().mockReturnValue(true) })
       .compile();
 
-    authController = app.get<AuthController>(AuthController);
-    authService = app.get<AuthService>(AuthService);
-    userRepository = app.get<UserRepository>(UserRepository);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
+    jwtStrategy = module.get<JwtStrategy>(JwtStrategy);
   });
 
   beforeEach(() => {
@@ -147,6 +122,144 @@ describe('AuthController', () => {
         message: 'register success',
         data: defaultUserResponseDto,
       });
+    });
+  });
+
+  describe('logout function', () => {
+    let res: any;
+
+    beforeEach(() => {
+      res = httpMocks.createResponse();
+      jest.clearAllMocks();
+
+      // Mock AuthService
+      jest.spyOn(authService, 'logout').mockResolvedValue({
+        token: '',
+        path: '/',
+        httpOnly: true,
+        maxAge: 0,
+      });
+    });
+
+    it('should handle missing token', async () => {
+      const req = httpMocks.createRequest();
+
+      // Mock JwtStrategy to throw error
+      const error = ServiceException.UnAuthorizedException(
+        errorMessages.PROTECT_ROUTES,
+      );
+      jest.spyOn(jwtStrategy, 'checkTokenExists').mockImplementation(() => {
+        throw error;
+      });
+
+      try {
+        await jwtStrategy.validate(req);
+        await authController.logout(res);
+      } catch (e) {
+        expect(e).toBeInstanceOf(ServiceException);
+        expect(e.message).toBe(errorMessages.PROTECT_ROUTES);
+      }
+
+      expect(authService.logout).not.toHaveBeenCalled();
+    });
+
+    it('should handle expired token', async () => {
+      const req = httpMocks.createRequest();
+      const error = ServiceException.UnAuthorizedException(
+        errorMessages.TOKEN_EXPIRED,
+      );
+
+      jest
+        .spyOn(jwtStrategy, 'checkTokenExists')
+        .mockReturnValue('expired-token');
+      jest.spyOn(jwtStrategy, 'verifyAndDecodeToken').mockImplementation(() => {
+        throw error;
+      });
+
+      try {
+        await jwtStrategy.validate(req);
+        await authController.logout(res);
+      } catch (e) {
+        expect(e).toBeInstanceOf(ServiceException);
+        expect(e.message).toBe(errorMessages.TOKEN_EXPIRED);
+      }
+
+      expect(jwtStrategy.checkTokenExists).toHaveBeenCalled();
+      expect(jwtStrategy.verifyAndDecodeToken).toHaveBeenCalledWith(
+        'expired-token',
+      );
+      expect(authService.logout).not.toHaveBeenCalled();
+    });
+
+    it('should handle non-existent user', async () => {
+      const req = httpMocks.createRequest();
+      const error = ServiceException.UnAuthorizedException(
+        errorMessages.ENTITY_NOT_FOUND('User', '9999'),
+      );
+
+      jest
+        .spyOn(jwtStrategy, 'checkTokenExists')
+        .mockReturnValue('valid-token');
+      jest
+        .spyOn(jwtStrategy, 'verifyAndDecodeToken')
+        .mockResolvedValue({ id: 9999 });
+      jest.spyOn(jwtStrategy, 'checkUserExistsInDB').mockImplementation(() => {
+        throw error;
+      });
+
+      try {
+        await jwtStrategy.validate(req);
+        await authController.logout(res);
+      } catch (e) {
+        expect(e).toBeInstanceOf(ServiceException);
+        expect(e.message).toBe(errorMessages.ENTITY_NOT_FOUND('User', '9999'));
+      }
+
+      expect(jwtStrategy.checkTokenExists).toHaveBeenCalled();
+      expect(jwtStrategy.verifyAndDecodeToken).toHaveBeenCalledWith(
+        'valid-token',
+      );
+      expect(jwtStrategy.checkUserExistsInDB).toHaveBeenCalledWith(9999);
+      expect(authService.logout).not.toHaveBeenCalled();
+    });
+
+    it('should handle password changed scenario', async () => {
+      const req = httpMocks.createRequest();
+      const error = ServiceException.UnAuthorizedException(
+        errorMessages.USER_CHANGED_PASSWORD,
+      );
+
+      jest
+        .spyOn(jwtStrategy, 'checkTokenExists')
+        .mockReturnValue('valid-token');
+      jest.spyOn(jwtStrategy, 'verifyAndDecodeToken').mockResolvedValue({
+        id: defaultUser.id,
+        iat: Date.now() / 1000,
+      });
+      jest
+        .spyOn(jwtStrategy, 'checkUserExistsInDB')
+        .mockResolvedValue(defaultUser);
+      jest
+        .spyOn(jwtStrategy, 'checkUserPasswordChanged')
+        .mockImplementation(() => {
+          throw error;
+        });
+
+      try {
+        await jwtStrategy.validate(req);
+        await authController.logout(res);
+      } catch (e) {
+        expect(e).toBeInstanceOf(ServiceException);
+        expect(e.message).toBe(errorMessages.USER_CHANGED_PASSWORD);
+      }
+
+      expect(jwtStrategy.checkTokenExists).toHaveBeenCalledTimes(1);
+      expect(jwtStrategy.verifyAndDecodeToken).toHaveBeenCalledWith(
+        'valid-token',
+      );
+      expect(jwtStrategy.checkUserExistsInDB).toHaveBeenCalled();
+      expect(jwtStrategy.checkUserPasswordChanged).toHaveBeenCalled();
+      expect(authService.logout).not.toHaveBeenCalled();
     });
   });
 
