f77acd123c5b59abf9b31129a6d9c925d40be747
HARIAN Elyoth <harian.elyoth@outlook.com>
2025-05-24 14:32:28 +0900

    ci: hide env variables in the pipelines

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,28 +8,28 @@ npm run db:seed
 
 # Check the TYPE environment variable
 case ${TYPE:-production} in
-  "unit")
-    echo "Running unit tests..."
-    npm run test:unit
-    ;;
-  "e2e")
-    echo "Running e2e tests..."
-    npm run test:e2e
-    ;;
-  "coverage")
-    echo "Running coverage tests..."
-    npm run test:cov
-    ;;
-  "production")
-    echo "Starting production server..."
-    npm run start:prod
-    ;;
-  "development")
-    echo "Starting development server..."
-    npm run start:dev
-    ;;
-  *)
-    echo "Unknown TYPE: ${TEST_TYPE}"
-    exit 1
-    ;;
+ "unit")
+   echo "Running unit tests..."
+   npm run test:unit
+   ;;
+ "e2e")
+   echo "Running e2e tests..."
+   npm run test:e2e
+   ;;
+ "coverage")
+   echo "Running coverage tests..."
+   npm run test:cov
+   ;;
+ "production")
+   echo "Starting production server..."
+   npm run start:prod
+   ;;
+ "development")
+   echo "Starting development server..."
+   npm run start:dev
+   ;;
+ *)
+   echo "Unknown TYPE: ${TEST_TYPE}"
+   exit 1
+   ;;
 esac
