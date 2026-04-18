9404702a8e45ac01b32707c48889369ef830b19e
Harian Elyoth <32650912+Harian-Elyoth@users.noreply.github.com>
2026-04-18 22:32:13 +0900

    feat(project): add like/join-request error messages and UpdateJoinRequest DTO

--- src/common/enums/notificationMessages.ts
+++ src/common/enums/notificationMessages.ts
@@ -4,6 +4,8 @@ export const notificationMessages = {
     `Hey, someone wants to join the project ${projectTitle}!`,
   ACCEPT_JOIN_REQUEST: (projectTitle: string) =>
     `Hey, your join request to the project ${projectTitle} has been accepted!`,
+  REJECT_JOIN_REQUEST: (projectTitle: string) =>
+    `Hey, your join request to the project ${projectTitle} has been rejected.`,
   INVITE_TO_PROJECT: (projectTitle: string) =>
     `Hey, you have been invited to the project ${projectTitle}!`,
 };
